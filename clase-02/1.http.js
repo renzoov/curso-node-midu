75885f633a38c4a9aacfb57c7ea77aa2949c76b2
Renzo Osorio <renzo.osorio1909@gmail.com>
2023-09-29 22:14:38 -0500

    agregar clase-02

---
+++ clase-02/1.http.js
@@ -0,0 +1,22 @@
+const http = require('node:http')
+
+const desiredPort = process.env.PORT ?? 1234
+
+const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+  if (req.url === '/') {
+    res.end('<h1>Mi página</h1>')
+  } else if (req.url === '/contacto') {
+    res.end('<h1>Contacto</h1>')
+  } else {
+    res.statusCode = 404
+    res.end('<h1>404</h1>')
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`)
+})
